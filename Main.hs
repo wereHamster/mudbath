62a76b4181e87a7feb4990f75c0a6d08c4766246
Tomas Carnecky <tomas.carnecky@gmail.com>
2015-05-31 13:26:54 +0200

    Use github-webhook-handler-snap to implement the bulk of the webhook handling

--- Main.hs
+++ Main.hs
@@ -25,6 +25,9 @@ import           Control.Exception
 import           System.Environment
 
 import           GitHub.Types
+import           GitHub.WebHook.Handler
+import           GitHub.WebHook.Handler.Snap
+
 import           Executor
 import           Notifications
 
@@ -45,7 +48,7 @@ main = do
         backgroundBuildThread queue `catch` ignoreException
 
     quickHttpServe $
-        path (BC8.pack hookPath) (method POST $ hook queue mbSecretKey) <|> writeText "ok\n"
+        webhookHandler (BC8.pack hookPath) mbSecretKey (handleEvent queue) <|> writeText "ok\n"
 
   where
 
@@ -65,58 +68,13 @@ main = do
 --
 -- Some events are processed immediately, others are put into the queuer to
 -- process them in the background thread.
-hook :: TQueue Event -> Maybe String -> Snap ()
-hook queue mbSecretKey = do
-    hdrs <- headers <$> getRequest
-
-    body <- do
-        body <- readRequestBody (100 * 1000)
-        case (mbSecretKey, getHeader "X-Hub-Signature" hdrs) of
-
-            -- No secret key and no signature. Pass along the body unverified.
-            (Nothing, Nothing) -> return body
-
-            -- Signature is available but no secret key to verify it. Log this
-            -- and accept the body.
-            (Nothing, Just _) -> do
-                liftIO $ putStrLn $
-                    "Ignoring signature because the secret token was not provided"
-                return body
-
-            -- Secret token is available but the request is not signed. Reject
-            -- the request.
-            (Just _, Nothing) -> do
-                writeText "Signature missing"
-                res <- getResponse
-                finishWith $ setResponseCode 400 res
-
-            -- Both the signature and secret token are available. Verify the
-            -- signature and reject the request if that fails.
-            (Just sc, Just sig) -> do
-                let mac = hmac (BC8.pack sc) (LBS.toStrict body) :: HMAC SHA1
-                if sig == ("sha1=" <> digestToHexByteString (hmacGetDigest mac))
-                    then return body
-                    else do
-                        liftIO $ do
-                            putStrLn "Signature does not match:"
-                            print $ show sig
-                            print $ digestToHexByteString $ hmacGetDigest mac
-
-                        writeText "Signature does not match"
-                        res <- getResponse
-                        finishWith $ setResponseCode 400 res
-
-
-    let mbEvent = do
-        eventName <- getHeader "X-GitHub-Event" hdrs
-        value     <- decode body
-        parseMaybe (eventParser $ decodeUtf8 eventName) value
-
-    case mbEvent of
-        Nothing -> return ()
-        Just ev -> void $ liftIO $ case ev of
-            (DeploymentEventType _) -> atomically $ writeTQueue queue ev
-            _                       -> processEvent ev
+hook :: TQueue Event -> Either Error Event -> Snap ()
+hook queue res = case res of
+    Left _ -> do
+        writeText "error\n"
+    Right ev -> liftIO $ case ev of
+        (DeploymentEventType _) -> atomically $ writeTQueue queue ev
+        _                       -> processEvent ev
 
 
 
